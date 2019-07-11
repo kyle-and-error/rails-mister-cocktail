d14e47e55e82071eb717621fd3f967b4b1f08dec
Kyle Cho <kylescho99@gmail.com>
2019-07-11 10:41:03 +0200

    Models

---
+++ db/migrate/20190711084020_create_doses.rb
@@ -0,0 +1,10 @@
+class CreateDoses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :doses do |t|
+      t.text :description
+      t.integer :cocktail_id
+
+      t.timestamps
+    end
+  end
+end
