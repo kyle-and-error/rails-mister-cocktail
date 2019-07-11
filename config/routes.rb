75e51b769af9b67575c9f63da3fc7424185b45fe
Kyle Cho <kylescho99@gmail.com>
2019-07-11 12:16:42 +0200

    dose controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'dose/new'
+  get 'dose/create'
   get 'cocktails/show'
   get 'cocktails/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: :new
+  end
 end
