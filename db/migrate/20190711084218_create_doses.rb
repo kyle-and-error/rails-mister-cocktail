c00a5ce0055c9a52d08e7a683bc691b4623d741e
Kyle Cho <kylescho99@gmail.com>
2019-07-11 10:45:08 +0200

    added ingredient column to dose

--- db/migrate/20190711084218_create_doses.rb
+++ db/migrate/20190711084218_create_doses.rb
@@ -3,6 +3,7 @@ class CreateDoses < ActiveRecord::Migration[5.2]
     create_table :doses do |t|
       t.text :description
       t.integer :cocktail_id
+      t.integer :ingredient_id
 
       t.timestamps
     end
