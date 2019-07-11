a41d045bd9ec76372c9a2f539b2348e32ef7c637
Kyle Cho <kylescho99@gmail.com>
2019-07-11 12:27:45 +0200

    rename dose controller to doses controller

--- app/controllers/dose_controller.rb
+++
@@ -1,7 +0,0 @@
-class DoseController < ApplicationController
-  def new
-  end
-
-  def create
-  end
-end
---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,25 @@
+class DoseController < ApplicationController
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+
+    respond_to do |format|
+      if @dose.save
+        format.html { redirect_to @dose, notice: 'dose was successfully created.' }
+        format.json { render :show, status: :created, location: @dose }
+      else
+        format.html { render :new }
+        format.json { render json: @dose.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
