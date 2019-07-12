4d2062c0b1ac2716140ff788e00c6b093781f26c
Kyle Cho <kylescho99@gmail.com>
2019-07-12 16:44:35 +0200

    front end and one small fix

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -15,6 +15,12 @@ class DosesController < ApplicationController
     end
   end
 
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
+  end
+
   private
 
   def set_cocktail
