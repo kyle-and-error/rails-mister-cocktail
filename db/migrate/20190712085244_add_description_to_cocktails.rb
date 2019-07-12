4cbbdbb78dedeef0eae817077fdfcee3ddf56ca4
Kyle Cho <kylescho99@gmail.com>
2019-07-12 10:57:21 +0200

    views and migrations

---
+++ db/migrate/20190712085244_add_description_to_cocktails.rb
@@ -0,0 +1,4 @@
+class AddDescriptionToCocktails < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
---
+++ db/migrate/20190712085300_add_img_to_cocktails.rb
@@ -0,0 +1,4 @@
+class AddImgToCocktails < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
